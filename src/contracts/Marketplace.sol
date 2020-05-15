a230cafda2beea0b229183d8bbfd18da9c6f52ff
Nate <nathan.wangemann@aggiemail.usu.edu>
2020-05-14 22:16:54 -0700

    sets up logic for creating a product, and tests that verify that this function works correctly

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -2,6 +2,8 @@ pragma solidity ^0.5.0;
 
 contract Marketplace {
     string public name;
+    uint public productCount = 0;
+    mapping(uint => Product) public products;
 
     struct Product {
         uint id;
@@ -11,7 +13,23 @@ contract Marketplace {
         bool purchased;
     }
 
+    event ProductCreated(
+          uint id,
+        string name,
+        uint price,
+        address owner,
+        bool purchased
+    );
+
     constructor() public {
         name = "Exploration Marketplace";
+    }
+
+    function createProduct(string memory _name, uint _price) public {
+        require(bytes(_name).length > 0);
+        require(_price > 0);
+        productCount++;
+        products[productCount] = Product(productCount, _name, _price, msg.sender, false);
+        emit ProductCreated(productCount, _name, _price, msg.sender, false);
     }
 }
--- test/Marketplace.test.js
+++ test/Marketplace.test.js
@@ -1,6 +1,10 @@
 const Marketplace = artifacts.require('./Marketplace.sol')
 
-contract('Marketplace', (accounts) => {
+require('chai')
+    .use(require('chai-as-promised'))
+    .should()
+
+contract('Marketplace', ([deployer, seller, buyer]) => {
     let marketplace
 
     before(async () => {
@@ -19,6 +23,32 @@ contract('Marketplace', (accounts) => {
         it('has a name', async () => {
             const name = await marketplace.name()
             assert.equal(name, 'Exploration Marketplace')
+        })
+    })
+
+    describe('products', async() => {
+        let result, productCount
+
+        before(async () => {
+            result = await marketplace.createProduct('test Product', web3.utils.toWei('1', 'Ether'), { from: seller })
+            productCount = await marketplace.productCount()
+        })
+    
+
+        it('creates products', async () => {
+            //success
+            assert.equal(productCount, 1)
+            const event = result.logs[0].args
+            assert.equal(event.id.toNumber(), productCount.toNumber(), 'id is correct')
+            assert.equal(event.name, 'test Product', 'name is correct')
+            assert.equal(event.price,  web3.utils.toWei('1', 'Ether'), 'price is correct')
+            assert.equal(event.owner, seller, 'owner is correct')
+            assert.equal(event.purchased, false, 'purchased is correct')
+
+            //failure
+            await await marketplace.createProduct('', web3.utils.toWei('1', 'Ether'), {from: seller}).should.be.rejected;
+            await await marketplace.createProduct('test Product', 0, {from: seller}).should.be.rejected;
+
         })
     })
 })
