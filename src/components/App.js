704596efffa8ea2c6d3248187b9ef95c8c318f54
Nate <nathan.wangemann@aggiemail.usu.edu>
2020-05-15 13:00:36 -0700

    adds functionality for selling a product on the marketplace and adding an item to the marketplace

--- src/components/App.js
+++ src/components/App.js
@@ -1,86 +1,77 @@
-import React, { Component } from 'react';
-import logo from '../logo.png';
-import './App.css';
-import Web3 from 'web3';
-import Marketplace from '../abis/Marketplace.json'
-import NavBar from '../components/NavBar'
+import React, { Component } from "react";
+import "./App.css";
+import Web3 from "web3";
+import Marketplace from "../abis/Marketplace.json";
+import NavBar from "../components/NavBar";
+import Main from "../components/Main";
 
 class App extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      account: '',
+      account: "",
       productCount: 0,
       products: [],
-      loading: true
-    }
+      loading: true,
+    };
   }
 
-  async componentWillMount(){
-    await this.loadWeb3()
-    await this.loadBlockchainData()
-  }
-
-  async loadWeb3(){
-    if(window.ethereum){
-      window.web3 = new Web3(window.ethereum)
-      await window.ethereum.enable()
-    } else if (window.web3){
-      window.web3 = new Web3(window.web3.currentProvider)
-    } else {
-      window.alert('Non-Ethereum browser detected. Try again buckaroo')
-    }
-  }
-
-  async loadBlockchainData(){
-    const web3 = window.web3
-    //load account
-    const accounts = await web3.eth.getAccounts()
-    console.log('accounts', accounts)
-    this.setState({
-      account: accounts[0]
-    })
-    const networkId = await web3.eth.net.getId()
-    const networkData = Marketplace.networks[networkId]
-    if(networkData){
-      const marketplace = web3.eth.Contract(Marketplace.abi, networkData.address)
-      this.setState({ marketplace: marketplace })
+  createProduct = (name, price) => {
+    this.setState({ loading: true })
+    this.state.marketplace.methods.createProduct(name, price).send({from:this.state.account})
+    .once('receipt', (receipt) => {
       this.setState({ loading: false })
-    } else {
-      window.alert('Marketplace not deployed to detected network')
-    }
-  
+    })
   }
-  
+
+  async componentWillMount() {
+    await this.loadWeb3();
+    await this.loadBlockchainData();
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert("Non-Ethereum browser detected. Try again buckaroo");
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3;
+    //load account
+    const accounts = await web3.eth.getAccounts();
+    console.log("accounts", accounts);
+    this.setState({
+      account: accounts[0],
+    });
+    const networkId = await web3.eth.net.getId();
+    const networkData = Marketplace.networks[networkId];
+    if (networkData) {
+      const marketplace = web3.eth.Contract(
+        Marketplace.abi,
+        networkData.address
+      );
+      this.setState({ marketplace: marketplace });
+      const productCount = await marketplace.methods.productCount().call()
+      console.log(productCount)
+      this.setState({ loading: false });
+    } else {
+      window.alert("Marketplace not deployed to detected network");
+    }
+  }
+
   render() {
     return (
       <div>
         <NavBar account={this.state.account} />
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <h1 className="navbar-brand col-sm-3 col-md-2 mr-0">
-            Blockchain Exploration Marketplace
-          </h1>
-          <ul className="navbar-nav px-3">
-            <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
-              <small className="text-white">
-    <span id="account">{this.state.account}</span>
-              </small>
-            </li>
-          </ul>
-        </nav>
         <div className="container-fluid mt-5">
           <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Blockchain Exploration Marketplace</h1>
-              </div>
+            <main role="main" className="col-lg-12 d-flex">
+              { this.state.loading ? <div id="loader" className="text-center"><p className="text-center">Loading...</p></div>: <Main createProduct={this.createProduct} />}
             </main>
           </div>
         </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,31 +1,22 @@
-import React, { Component } from 'react';
+import React from "react";
 
-
-class NavBar extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-
-    }
-  }
-  render() {
-    return (
-      <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <h1 className="navbar-brand col-sm-3 col-md-2 mr-0">
-            Blockchain Exploration Marketplace
-          </h1>
-          <ul className="navbar-nav px-3">
-            <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
-              <small className="text-white">
-    <span id="account">{this.props.account}</span>
-              </small>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    );
-  }
+function NavBar(props) {
+  return (
+    <div>
+      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+        <h1 className="navbar-brand col-sm-3 col-md-2 mr-0">
+          Blockchain Exploration Marketplace
+        </h1>
+        <ul className="navbar-nav px-3">
+          <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
+            <small className="text-white">
+              <span id="account">{props.account}</span>
+            </small>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
 export default NavBar;
