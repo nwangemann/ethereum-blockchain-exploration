f788d55cc2eaf4951f6ac76537613b7a8cb5983b
Nate <nathan.wangemann@aggiemail.usu.edu>
2020-05-14 21:22:13 -0700

    created new product structure

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -3,6 +3,14 @@ pragma solidity ^0.5.0;
 contract Marketplace {
     string public name;
 
+    struct Product {
+        uint id;
+        string name;
+        uint price;
+        address owner;
+        bool purchased;
+    }
+
     constructor() public {
         name = "Exploration Marketplace";
     }
